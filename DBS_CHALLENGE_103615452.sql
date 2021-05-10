bce825281d9301eb4e63cf26c4912df50c1722a8
Agam Singh <79820189+Agam-S@users.noreply.github.com>
2021-05-10 10:25:15 +1000

    Task 2 Complete

--- DBS_CHALLENGE_103615452.sql
+++ DBS_CHALLENGE_103615452.sql
@@ -19,6 +19,42 @@ Primary Key (TourName, EventYear, EventMonth, EventDay)
 Foreign Key TourName REFERENCES Tour */
 
 
+CREATE TABLE Tour (
+  TourName   NVARCHAR(100) 
+, DESCRIPTION    NVARCHAR(500)
+, PRIMARY KEY (TourName)
+);
+
+CREATE TABLE Client (
+  ClientID   INT 
+, Surname   NVARCHAR(100) 
+, GivenName   NVARCHAR(100)
+, Gender      NVARCHAR(1) 
+, PRIMARY KEY  (ClientID) 
+);
+
+CREATE TABLE Event (
+  TourName  NVARCHAR(100)
+, EventYear   INT 
+, EventMonth   NVARCHAR(3)
+, EventDay      INT
+, Fee      MONEY 
+, PRIMARY KEY  (TourName, EventYear, EventMonth, EventDay) 
+, FOREIGN KEY (TourName) REFERENCES Tour
+);
+
+CREATE TABLE Booking (
+  ClientID  INT
+, TourName  NVARCHAR(100)
+, EventYear   INT 
+, EventMonth   NVARCHAR(3)
+, EventDay      INT
+, Payment      MONEY 
+, DateBooked   DATE
+, PRIMARY KEY  (ClientID, TourName, EventYear, EventMonth, EventDay)
+, FOREIGN KEY (ClientID) REFERENCES Client
+, FOREIGN KEY  (TourName, EventYear, EventMonth, EventDay) REFERENCES Event
+);
 
 
 
